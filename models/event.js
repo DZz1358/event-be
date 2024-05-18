9a9e5b59e0d0eca52d8d9afb5c74d90b75eae942
Den <saloed2206@gmail.com>
2024-05-18 09:17:15 +0300

    added methods for event

---
+++ models/event.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    attendees: [{
+        name: String,
+        email: String
+    }]
+}, {timestamps: true})
+
+
+const Event =  mongoose.model('Event', eventSchema)
+
+module.exports = Event;
