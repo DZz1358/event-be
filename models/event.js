cd0715a734483bd9f9331a5abfad3fcd6ea0cfb1
Den <saloed2206@gmail.com>
2024-05-19 11:08:03 +0300

    update event model

--- models/event.js
+++ models/event.js
@@ -6,6 +6,14 @@ const eventSchema = new Schema({
         type: String,
         required: true,
     },
+    author: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: String,
+        required: true,
+    },
     description: {
         type: String,
         required: true,
